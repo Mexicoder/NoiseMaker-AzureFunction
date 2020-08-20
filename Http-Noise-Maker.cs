2be105e8730ddac507014b0fe9b0a71ee8b103b8
John Friesen <john.b.friesen@accenture.com>
2020-08-20 17:19:56 -0400

    upload code

---
+++ Http-Noise-Maker.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class Http_Noise_Maker
+    {
+        ///
+        /// Add query string parameters to change the http code returned
+        /// ?pass=true
+        /// ?notfound=true
+        /// Default return is 418 i'm a tea pot
+        ///
+        [FunctionName("Http_Noise_Maker")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+             log.LogInformation("C# HTTP trigger function *NoiceMaker* processed a request.");
+
+            string pass = req.Query["pass"];
+            string notfound = req.Query["notfound"];
+
+            if (pass == "true")
+            {
+                var result = new ObjectResult("I'M OK 😁");
+                result.StatusCode = StatusCodes.Status200OK;
+                return result;
+            }
+            else if (notfound == "true")
+            {
+                var result = new ObjectResult("I'M NOT FOUND 😲");
+                result.StatusCode = StatusCodes.Status404NotFound;
+                return result;
+            }
+            else
+            {
+                var result = new ObjectResult("I'M A TEA POT 🍵");
+                result.StatusCode = StatusCodes.Status418ImATeapot;
+                return result;
+            }
+        }
+    }
+}
